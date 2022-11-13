862c5ad9c4a2db54718a8a2ce554672c6adf6115
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-11-13 17:51:09 +0700

    adding models file

--- Tabas/__manifest__.py
+++ Tabas/__manifest__.py
@@ -1,7 +1,18 @@
 {
     'name':"My student - phuong.info", #tên module
     'summary':"""My student model""",#mô tả ngắn gọn về module
-    'description':"""Managing pet infomation""",
+    'description':"""Managing student infomation""",
     'author':"hoang-phuong",
+    'website':"https://studentapp.info",
+    'catagory':"Uncategorized",
+    'version':"0.1",
+    'depends':[
+        'product'
+    ],# dependency của module mình sẽ phụ thuộc vào những app/module khác nào.product là một module built-in đặc tả khi cài đặt My student sẽ trigger cài đặt product
+    'data':[],#liên quan đến view
+    'installable':True,
+    'application':True # khi vào menu Apps, mặc định filter "Apps" sẽ được dùng.Nếu application set false => ko hiện ra khi filter "App",ngược lại được xem nh7 1 application.Application False khi module này là như một module kỹ thuật phụ trợ
+
+
 
 }
---
+++ Tabas/models/student.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, tools, _
+from odoo.exceptions import UserError, ValidationError
+
+class MyStudent(models.Model):
+    _name = "my.Student"
+    _description = "My student model"
+
+    name = fields.Char('Student Name', required=True) #Char: text 1 dòng
+    nickname = fields.Char('Nickname')
+    description = fields.Text('student Description')#Text: textarea nhiều dòng
+    age = fields.Integer('student Age', default=6)#Integer: số nguyên
+    weight = fields.Float('Weight (kg)')#Float: số thực
+    dob = fields.Date('DOB', required=False)#Date: ngày tháng năm
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('female', 'Female')
+    ], string='Gender', default='male')#Selection: chọn lựa, option
+    student_image = fields.Binary("Student Image", attachment=True, help="Student Image")#Binary: lưu ảnh
+    owner_id = fields.Many2one('res.partner', string='Owner')#Many2one: quan hệ nhiều một: N-1
+    product_ids = fields.Many2many(comodel_name='product.product',
+                                string="Related Products",
+                                relation='student_product_rel',
+                                column1='col_student_id',
+                                column2='col_product_id')#quan hệ nhiều nhiều: N-N
