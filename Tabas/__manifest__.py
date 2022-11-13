2218510657b1fb7ef4d3999fa4ba97a346b88da6
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-11-13 17:24:31 +0700

    studentmanamentapp

---
+++ Tabas/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name':"My student - phuong.info", #tên module
+    'summary':"""My student model""",#mô tả ngắn gọn về module
+    'description':"""Managing pet infomation""",
+    'author':"hoang-phuong",
+
+}
