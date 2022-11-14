adfa6a5b9a553334ae0d2925d377eeee662b3713
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-11-14 18:14:10 +0700

    change folder name

--- Mystudentapp/__manifest__.py
+++ Mystudentapp/__manifest__.py
@@ -11,8 +11,12 @@
     ],# dependency của module mình sẽ phụ thuộc vào những app/module khác nào.product là một module built-in đặc tả khi cài đặt My student sẽ trigger cài đặt product
     'data':[],#liên quan đến view
     'installable':True,
-    'application':True # khi vào menu Apps, mặc định filter "Apps" sẽ được dùng.Nếu application set false => ko hiện ra khi filter "App",ngược lại được xem nh7 1 application.Application False khi module này là như một module kỹ thuật phụ trợ
+    'application':True, # khi vào menu Apps, mặc định filter "Apps" sẽ được dùng.Nếu application set false => ko hiện ra khi filter "App",ngược lại được xem nh7 1 application.Application False khi module này là như một module kỹ thuật phụ trợ
+    'data': [
+        'security/ir.model.access.csv',
+        'views/my_student_views.xml',
 
+    ],
 
 
 }
